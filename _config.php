4dfcf73fed30750591146f7f1eb1fa012b2d998a
v2 <v2@v2.local>
2019-12-11 13:28:27 +1300

    routes

--- _config.php
+++ _config.php
@@ -6,28 +6,22 @@ use SilverStripe\View\Parsers\ShortcodeParser;
 use SilverStripe\View\Requirements;
 use CatchDesign\EmbedableGallery\Extensions\SSEmedableGalleryPageExtension;
 
-
-
-$module = ModuleLoader::inst()->getManifest()->getModule('catchdesign/embedable-gallery');
-
 // Define path constant
 $path = str_replace('\\', '/', __DIR__);
 $path_fragments = explode('/', $path);
 $dir_name = $path_fragments[count($path_fragments) - 1];
+define('SS_EMBEDABLE_GALLERY_NS', 'catch/ss-embedable-gallery:');
 define('SS_EMBEDABLE_GALLERY_DIR', $dir_name);
 define('SS_EMBEDABLE_GALLERY_PATH', __DIR__);
 $cmsConfig = HTMLEditorConfig::get('cms');
-
+$loader = ModuleResourceLoader::singleton();
 // page Extension
 \Page::add_extension(SSEmedableGalleryPageExtension::class);
 
 
 // Requirements::css('silverstripe-example-module/styles/admin.css');
 // + Requirements::css('example-user/silverstripe-example-module: styles/admin.css');
-
-
-// require
-Requirements::javascript( $module->getResource('assets/build/js/lib.js') );
+Requirements::javascript( SS_EMBEDABLE_GALLERY_NS . 'assets/build/js/lib.js');
 
 // add the embed functionality
 ShortcodeParser::get('default')->register(
@@ -35,13 +29,13 @@ ShortcodeParser::get('default')->register(
 	['SSEmedableGalleryPageExtension', 'AlbumEmbedParser']
 );
 $cmsConfig->enablePlugins(array(
-	'albumEmbed' => $module->getResource('assets/build/js/editor-plugin.js')
+	'albumEmbed' => $loader->resolveURL(SS_EMBEDABLE_GALLERY_NS . 'assets/build/js/editor-plugin.js')
 ));
 $cmsConfig->addButtonsToLine(2, 'albumEmbed');
 
 $styleSheets = $cmsConfig->getOption('content_css');
 $cmsConfig->setContentCSS([
-    $module->getResource('assets/build/css/main.css')
+    $loader->resolveURL(SS_EMBEDABLE_GALLERY_NS . 'assets/build/css/main.css')
 ]);
 
 // HtmlEditorConfig::get('cms')->setOption(
