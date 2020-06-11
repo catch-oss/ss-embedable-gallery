48f1207d9305060dea34d8dc7dcca02cfd9d8220
Sam Etheridge <sam@etheridge.co.nz>
2020-06-11 17:17:04 +1200

    PHP CS FIXER

--- code/Controllers/AlbumAdmin.php
+++ code/Controllers/AlbumAdmin.php
@@ -3,54 +3,63 @@
 namespace CatchDesign\EmbedableGallery\Controllers;
 
 use CatchDesign\EmbedableGallery\SiteTree\AlbumPage;
+use SilverStripe\Control\Controller;
+use SilverStripe\Core\Convert;
 use SilverStripe\Security\Permission;
 use SilverStripe\Security\Security;
-use SilverStripe\Core\Convert;
-use SilverStripe\Control\Controller;
 
-class AlbumAdmin extends Controller {
+class AlbumAdmin extends Controller
+{
+    private static $allowed_actions = [
+        'index',
+        'albums',
+        'htmlfragment',
+    ];
 
-	private static $allowed_actions = array(
-		'index',
-		'albums',
-		'htmlfragment'
-	);
+    public function ModuleDir()
+    {
+        return SS_EMBEDABLE_GALLERY_DIR;
+    }
 
-	public function ModuleDir() {
-		return SS_EMBEDABLE_GALLERY_DIR;
-	}
+    public function init()
+    {
+        parent::init();
+        if (!Permission::check('CMS_ACCESS')) {
+            Security::permissionFailure();
+        }
+    }
 
-	public function init() {
-		parent::init();
-		if (!Permission::check('CMS_ACCESS')) Security::permissionFailure();
-	}
+    public function index()
+    {
+        return [];
+    }
 
-	public function index() {
-		return [];
-	}
+    public function albums()
+    {
 
-	public function albums() {
+        // look for albums
+        $q = $this->request->getVar('q');
+        $r = AlbumPage::get()->where('Title LIKE \'%' . Convert::raw2sql($q) . '%\'');
 
-		// look for albums
-		$q = $this->request->getVar('q');
-		$r = AlbumPage::get()->where('Title LIKE \'%' . Convert::raw2sql($q) . '%\'');
+        // build output
+        $out = [];
+        foreach ($r as $a) {
+            $out[] = [
+                'value' => $a->ID,
+                'text'  => $a->Title,
+            ];
+        }
 
-		// build output
-		$out = [];
-		foreach ($r as $a) {
-			$out[] = [
-				'value' => $a->ID,
-				'text' => $a->Title,
-			];
-		}
+        $this->response->addHeader('Content-Type', 'application/json');
 
-		$this->response->addHeader('Content-Type', 'application/json');
-		return json_encode($out);
-	}
+        return json_encode($out);
+    }
 
-	public function htmlfragment() {
-		$id = (int) $this->request->param('ID');
-		$a = AlbumPage::get()->filter(['ID' => $id])->first();
-		return $a->renderWith('EmbeddedAlbum');
-	}
+    public function htmlfragment()
+    {
+        $id = (int) $this->request->param('ID');
+        $a = AlbumPage::get()->filter(['ID' => $id])->first();
+
+        return $a->renderWith('EmbeddedAlbum');
+    }
 }
--- code/Extensions/SSEmedableGalleryPageExtension.php
+++ code/Extensions/SSEmedableGalleryPageExtension.php
@@ -4,28 +4,28 @@ namespace CatchDesign\EmbedableGallery\Extensions;
 
 use CatchDesign\EmbedableGallery\SiteTree\AlbumPage;
 use SilverStripe\AssetAdmin\Forms\UploadField;
-use SilverStripe\ORM\DataExtension;
 use SilverStripe\Assets\Image;
 use SilverStripe\Assets\Upload_Validator;
+use SilverStripe\ORM\DataExtension;
 
 /**
  * @todo need reconcile removals in both directions
  * @todo remove PublicationFBUpdateID && PublicationTweetID as they aren't really needed any more - if testing for post just call $this->owner->PublicationTweets()->count()
  */
-class SSEmedableGalleryPageExtension extends DataExtension {
-
+class SSEmedableGalleryPageExtension extends DataExtension
+{
     private static $casting = [
-        'AlbumEmbedParser' => 'HTMLText'
+        'AlbumEmbedParser' => 'HTMLText',
     ];
 
     private static $has_one = [
-        'PrimaryImage'      => Image::class
+        'PrimaryImage'      => Image::class,
     ];
 
     public function updateCMSFields(\SilverStripe\Forms\FieldList $fields)
     {
         // images
-        $iValidator = new Upload_Validator;
+        $iValidator = new Upload_Validator();
         $iValidator->setAllowedExtensions(['jpg', 'jpeg', 'gif', 'png']);
         $fields->AddFieldsToTab(
             'Root.Images',
@@ -40,14 +40,17 @@ class SSEmedableGalleryPageExtension extends DataExtension {
 
     /**
      * parses out short codes:
-     * [album_embed,id="123"]
+     * [album_embed,id="123"].
+     *
      * @param [type] $arguments [description]
      * @param [type] $content   [description]
      * @param [type] $parser    [description]
      * @param [type] $tagName   [description]
      */
-    public static function AlbumEmbedParser($arguments, $content = null, $parser = null, $tagName) {
+    public static function AlbumEmbedParser($arguments, $content = null, $parser = null, $tagName)
+    {
         $album = AlbumPage::get()->filter(['ID' => $arguments['id']])->first();
+
         return $album->renderWith('EmbeddedAlbum');
     }
 }
